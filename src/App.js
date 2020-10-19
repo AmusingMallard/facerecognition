f7b7f9b523f9b8ea993ad06a8a9b873c4d89af8f
dockeith <dockeith@users.noreply.github.com>
2020-10-19 14:45:55 +0200

    add register and signin forms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Particles from "react-particles-js";
 import Clarifai from "clarifai";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Navigation from "./components/Navigation/Navigation";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
@@ -10,7 +12,7 @@ import "./App.css";
 import "tachyons";
 
 const app = new Clarifai.App({
-  apiKey: "2c731c389a6d49a48682a059ef2ce547"
+  apiKey: "2c731c389a6d49a48682a059ef2ce547",
 });
 
 const particlesOptions = {
@@ -21,24 +23,24 @@ const particlesOptions = {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 }
+      image: { src: "img/github.svg", width: 100, height: 100 },
     },
     opacity: {
       value: 0.5,
       random: false,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
     },
     size: {
       value: 3,
       random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
     },
     line_linked: {
       enable: true,
       distance: 150,
       color: "#ffffff",
       opacity: 0.4,
-      width: 1
+      width: 1,
     },
     move: {
       enable: true,
@@ -48,23 +50,23 @@ const particlesOptions = {
       straight: false,
       out_mode: "out",
       bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 }
-    }
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
   },
   interactivity: {
     detect_on: "window",
     events: {
       onhover: { enable: true, mode: "repulse" },
-      resize: true
+      resize: true,
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
       repulse: { distance: 200, duration: 0.4 },
-      remove: { particles_nb: 2 }
-    }
+      remove: { particles_nb: 2 },
+    },
   },
-  retina_detect: true
+  retina_detect: true,
 };
 
 class App extends Component {
@@ -74,51 +76,76 @@ class App extends Component {
       input: "",
       imageUrl: "",
       box: {},
+      route: "signin",
+      isSignedIn: false,
     };
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height),
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   displayFaceBox = (box) => {
-    console.log(box);
-    this.setState({box});
-  }
+    this.setState({ box });
+  };
 
-  onInputChange = event => {
+  onInputChange = (event) => {
     this.setState({ input: event.target.value });
-  }
+  };
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(err => console.log(err));
-  }
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onSubmit={this.onSubmit}
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onSubmit={this.onSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
